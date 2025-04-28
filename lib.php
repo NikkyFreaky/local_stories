ed3ce59f8e50e83c08a89f757ac467a23147b476
Nikita Zlobin <nsz@2i.tusur.ru>
2025-04-28 21:09:25 +0700

    feat: add buttons to navbar

---
+++ lib.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package   local_stories
+ * @copyright 2025, Zlobin Nikita
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * @param \core_renderer $renderer The page renderer.
+ * @return string HTML for stories buttons.
+ */
+function local_stories_render_navbar_output(\core_renderer $renderer) {
+    // Данные для шаблона (если нужны динамические параметры).
+    $data = [];
+    return $renderer->render_from_template('local_stories/navbar', $data);
+} 
