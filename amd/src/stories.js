9aa1a59f6f6adfc3c141b60b8960bedcb4d175e9
Nikita Zlobin <nsz@2i.tusur.ru>
2025-04-29 20:08:54 +0700

    feat: minimal styles for preview photo/video and fix: preview for photo/video

--- amd/src/stories.js
+++ amd/src/stories.js
@@ -3,83 +3,67 @@ define(['jquery'], function ($) {
   return {
     init: function () {
       const modal = $('#stories-modal');
-      const fileInput = modal.find('.stories-file-input');
-      const dropzone = modal.find('.stories-dropzone');
-      const previewContainer = modal.find('.stories-preview-container');
-      const previewImage = modal.find('.stories-preview-image');
-      const previewVideo = modal.find('.stories-preview-video');
+      const fileInput = modal.find('.stories-editor__file-input');
+      const dropzone = modal.find('.stories-editor__dropzone');
+      const canvas = modal.find('.stories-editor__canvas');
+      const imgPreview = modal.find('.stories-editor__preview-image');
+      const videoPreview = modal.find('.stories-editor__preview-video');
+      const previewContainer = modal.find('.stories-editor__preview-container');
 
-      // Обработка выбора файла
+      /**
+       * Сбрасывает предпросмотр и состояние canvas
+       */
+      function resetPreview() {
+        imgPreview.attr('src', '').hide();
+        videoPreview.attr('src', '').hide();
+        previewContainer.hide();
+        dropzone.show();
+        canvas.removeClass('stories-editor__canvas--has-content');
+      }
+
+      /**
+       * Показывает предпросмотр для выбранного файла
+       * @param {File} file
+       */
+      function showPreview(file) {
+        resetPreview();
+        if (file.type.startsWith('image/')) {
+          imgPreview.attr('src', URL.createObjectURL(file)).show();
+        } else if (file.type.startsWith('video/')) {
+          videoPreview.attr('src', URL.createObjectURL(file)).show();
+        }
+        previewContainer.show();
+        dropzone.hide();
+        canvas.addClass('stories-editor__canvas--has-content');
+      }
+
       fileInput.on('change', function (e) {
         const file = e.target.files[0];
         if (file) {
-          handleFile(file);
+          showPreview(file);
         }
       });
 
-      // Обработка drag & drop
-      dropzone.on('dragover', function (e) {
+      // Drag&Drop
+      dropzone.on('dragenter dragover', function (e) {
         e.preventDefault();
-        $(this).addClass('stories-dropzone-active');
+        e.stopPropagation();
+        dropzone.addClass('stories-editor__dropzone--dragover');
       });
-
-      dropzone.on('dragleave', function (e) {
+      dropzone.on('dragleave dragend drop', function (e) {
         e.preventDefault();
-        $(this).removeClass('stories-dropzone-active');
+        e.stopPropagation();
+        dropzone.removeClass('stories-editor__dropzone--dragover');
       });
-
       dropzone.on('drop', function (e) {
-        e.preventDefault();
-        $(this).removeClass('stories-dropzone-active');
         const file = e.originalEvent.dataTransfer.files[0];
         if (file) {
-          handleFile(file);
+          showPreview(file);
         }
       });
 
-      /**
-       * Обрабатывает загруженный файл и отображает его превью
-       * @param {File} file Загруженный файл
-       */
-      function handleFile(file) {
-        const reader = new FileReader();
-
-        if (file.type.startsWith('image/')) {
-          reader.onload = function (e) {
-            previewImage.attr('src', e.target.result);
-            previewImage.removeClass('d-none');
-            previewVideo.addClass('d-none');
-            dropzone.addClass('d-none');
-            previewContainer.removeClass('d-none');
-          };
-          reader.readAsDataURL(file);
-        } else if (file.type.startsWith('video/')) {
-          const videoUrl = URL.createObjectURL(file);
-          previewVideo.attr('src', videoUrl);
-          previewVideo.removeClass('d-none');
-          previewImage.addClass('d-none');
-          dropzone.addClass('d-none');
-          previewContainer.removeClass('d-none');
-        }
-      }
-
-      // Обработка закрытия модального окна
-      modal.find('[data-action="close-modal"]').on('click', function () {
-        resetModal();
-      });
-
-      /**
-       * Сбрасывает состояние модального окна
-       */
-      function resetModal() {
-        fileInput.val('');
-        previewImage.attr('src', '');
-        previewVideo.attr('src', '');
-        previewImage.addClass('d-none');
-        previewVideo.addClass('d-none');
-        previewContainer.addClass('d-none');
-        dropzone.removeClass('d-none');
-      }
+      // Сброс при закрытии модалки
+      modal.find('[data-action="close-modal"]').on('click', resetPreview);
     },
   };
 });
